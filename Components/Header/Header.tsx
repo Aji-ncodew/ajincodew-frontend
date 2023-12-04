9a8efe4dae186a72a2070290f0f80cb0a8fb81f3
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2023-12-04 12:39:12 +0100

    new

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -1,105 +1,105 @@
-import classes from "./Header.module.css";
-import Image from "next/image";
-import NSCCLogo from "../../assets/logo.png";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-// import { onAuthStateChanged, OAuthCredential } from "firebase/auth";
-import { AiFillHome } from "react-icons/ai";
-import { MdEmojiEvents } from "react-icons/md";
-import { RiLoginCircleFill, RiLogoutCircleFill } from "react-icons/ri";
-import { useRouter } from "next/router";
-import PersonIcon from '@mui/icons-material/Person';
-// import { signOut ,sendEmailVerification} from "@firebase/auth";
-import { toast, ToastOptions } from "react-toastify";
-function Navbar() {
-  const toastOptions: ToastOptions = {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "dark",
-  };
-  const scrollThreshold = 40;
+  import classes from "./Header.module.css";
+  import Image from "next/image";
+  import NSCCLogo from "../../assets/logo.png";
+  import Link from "next/link";
+  import { useEffect, useState } from "react";
+  // import { onAuthStateChanged, OAuthCredential } from "firebase/auth";
+  import { AiFillHome } from "react-icons/ai";
+  import { MdEmojiEvents } from "react-icons/md";
+  import { RiLoginCircleFill, RiLogoutCircleFill } from "react-icons/ri";
+  import { useRouter } from "next/router";
+  import PersonIcon from '@mui/icons-material/Person';
+  // import { signOut ,sendEmailVerification} from "@firebase/auth";
+  import { toast, ToastOptions } from "react-toastify";
+  function Navbar() {
+    const toastOptions: ToastOptions = {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    };
+    const scrollThreshold = 40;
 
-  const router = useRouter();
-  const [active, setActive] = useState(false);
-  const [isScrolled, setIsScrolled] = useState(false);
+    const router = useRouter();
+    const [active, setActive] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
 
-  useEffect(() => {
-    const scrollHandler = () => {
-      const offset = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-      
-      setIsScrolled(offset > scrollThreshold);
-    }
+    useEffect(() => {
+      const scrollHandler = () => {
+        const offset = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        
+        setIsScrolled(offset > scrollThreshold);
+      }
 
-    window.addEventListener("scroll", scrollHandler);
+      window.addEventListener("scroll", scrollHandler);
 
-    // cleanup
-    return () => window.addEventListener("scroll", scrollHandler);
+      // cleanup
+      return () => window.addEventListener("scroll", scrollHandler);
 
-  }, [isScrolled]);
+    }, [isScrolled]);
 
-  const handlelogout = () => {
-    if (active) {
-      localStorage.removeItem("login");
-    }
-  };
+    const handlelogout = () => {
+      if (active) {
+        localStorage.removeItem("login");
+      }
+    };
 
-  return (
-    <div className={classes.container + (isScrolled ? " " + classes.scrolled : "")}>
-      <div className={classes.header}>
-        <Link href="/" className={classes.logo}>
-          <Image src={NSCCLogo} alt="NSCC Logo" width={100}/>
-        </Link>
+    return (
+      <div className={classes.container + (isScrolled ? " " + classes.scrolled : "")}>
+        <div className={classes.header}>
+          <Link href="/" className={classes.logo}>
+            <Image src={NSCCLogo} alt="NSCC Logo" width={100}/>
+          </Link>
+        </div>
+        <div className={classes.navigators}>
+          <ul>
+            <li>
+              <Link href="/">
+                <div className={classes.icons}>
+                  <AiFillHome />
+                </div>
+                <label style={{ cursor: "pointer" }}>Home</label>
+              </Link>
+            </li>
+            <li>
+              {/* <Link href="/events"> */}
+                <div className={classes.icons}>
+                  <MdEmojiEvents />
+                </div>
+                <label style={{ cursor: "pointer" }}>Events</label>
+              {/* </Link> */}
+            </li>
+            <li>
+              <Link href="/team">
+                <div className={classes.icons}>
+                  <PersonIcon style={{fontSize:"36px"}} />
+                </div>
+                <label style={{ cursor: "pointer" }}>Team</label>
+              </Link>
+            </li>
+            <li>
+              {/* <Link
+                onClick={handlelogout}
+                className={classes.button}
+                href={`/auth?redirect=${router.asPath.split('?')[0]}`}
+                > */}
+                <div className={classes.icons}>
+                  {active ? <RiLogoutCircleFill /> : <RiLoginCircleFill />}
+                </div>
+
+                <label  className={classes.button} style={{ cursor: "pointer" }} >{active ? "logout" : "login"}</label>
+
+              {/* </Link> */}
+              
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className={classes.navigators}>
-        <ul>
-          <li>
-            <Link href="/">
-              <div className={classes.icons}>
-                <AiFillHome />
-              </div>
-              <label style={{ cursor: "pointer" }}>Home</label>
-            </Link>
-          </li>
-          <li>
-            {/* <Link href="/events"> */}
-              <div className={classes.icons}>
-                <MdEmojiEvents />
-              </div>
-              <label style={{ cursor: "pointer" }}>Events</label>
-            {/* </Link> */}
-          </li>
-          <li>
-            <Link href="/team">
-              <div className={classes.icons}>
-                <PersonIcon style={{fontSize:"36px"}} />
-              </div>
-              <label style={{ cursor: "pointer" }}>Team</label>
-            </Link>
-          </li>
-          <li>
-            {/* <Link
-              onClick={handlelogout}
-              className={classes.button}
-              href={`/auth?redirect=${router.asPath.split('?')[0]}`}
-              > */}
-              <div className={classes.icons}>
-                {active ? <RiLogoutCircleFill /> : <RiLoginCircleFill />}
-              </div>
+    );
+  } /* achraf */
 
-              <label  className={classes.button} style={{ cursor: "pointer" }} >{active ? "logout" : "login"}</label>
-
-            {/* </Link> */}
-            
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-}
-
-export default Navbar;
+  export default Navbar;
